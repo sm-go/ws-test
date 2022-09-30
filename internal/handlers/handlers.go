66c570d8635af3731928374c94a7a5e5234a53e8
Smith <skkosmith@gmail.com>
2022-09-30 09:39:37 +0700

    installing the require packages

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,16 @@
+package handlers
+
+import "net/http"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// routing
+// go get github.com/bmizerany/pat
+
+// web socet
+// go get github.com/gorilla/websocket
+
+//rendering
+//go get github.com/CloudyKit/jet/v6
